3d492bce6928823e384a4d5e14b744f7c3a50150
Ahmad Wijaya <cibofdev@gmail.com>
2024-03-30 02:05:25 +0700

    opening the modal

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ModalService } from "../services/modal.service";
 
 @Component({
   selector: 'app-nav',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+
+  constructor(public modal: ModalService) {
+  }
+
+  openModal($event: Event) {
+    $event.preventDefault()
+
+    this.modal.toggleModal()
+  }
 
 }
