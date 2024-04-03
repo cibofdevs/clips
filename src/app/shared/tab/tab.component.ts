ca919d4abe0c571f8e84d2d1e04c01d4a0d3be51
Ahmad Wijaya <cibofdev@gmail.com>
2024-04-03 16:24:48 +0700

    setting an active tab

--- src/app/shared/tab/tab.component.ts
+++ src/app/shared/tab/tab.component.ts
@@ -7,4 +7,5 @@ import { Component, Input } from '@angular/core';
 })
 export class TabComponent {
   @Input() tabTitle = ""
+  @Input() active = false
 }
--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -10,6 +10,20 @@ export class TabsContainerComponent implements AfterContentInit {
   @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList()
 
   ngAfterContentInit() {
-    console.log(this.tabs)
+    const activeTabs = this.tabs?.filter(
+      tab => tab.active
+    )
+
+    if (!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first)
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach(tab => {
+      tab.active = false
+    })
+
+    tab.active = true
   }
 }
