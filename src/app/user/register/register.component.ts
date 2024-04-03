4559a3ccdc5a3a6532d904322cd39075cf473301
Ahmad Wijaya <cibofdev@gmail.com>
2024-04-04 06:20:10 +0700

    add form validation

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -8,7 +8,9 @@ import { FormGroup, FormControl } from "@angular/forms";
 })
 export class RegisterComponent {
   registerForm = new FormGroup({
-    name: new FormControl(''),
+    name: new FormControl('', [
+      Validators.required
+    ]),
     email: new FormControl(''),
     age: new FormControl(''),
     password: new FormControl(''),
