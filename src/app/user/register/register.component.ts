a2ea81bdb8825a5c7c424c2d3965b0b54d5de411
Ahmad Wijaya <cibofdev@gmail.com>
2024-04-04 05:51:18 +0700

    registering a new form

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerForm = new FormGroup({
 
+  })
 }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { SharedModule } from "../shared/shared.module";
 import { LoginComponent } from './login/login.component';
@@ -14,7 +15,8 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ],
   exports: [
     AuthModalComponent
