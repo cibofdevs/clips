e227d93464d586e1b7cc12329898417fb9411b78
Ahmad Wijaya <cibofdev@gmail.com>
2024-04-04 06:06:46 +0700

    binding a form

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup } from "@angular/forms";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -8,6 +8,11 @@ import { FormGroup } from "@angular/forms";
 })
 export class RegisterComponent {
   registerForm = new FormGroup({
-
+    name: new FormControl(''),
+    email: new FormControl(''),
+    age: new FormControl(''),
+    password: new FormControl(''),
+    confirm_password: new FormControl(''),
+    phoneNumber: new FormControl('')
   })
 }
