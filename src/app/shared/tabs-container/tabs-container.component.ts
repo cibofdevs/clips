3fb0b2a00ce64c9c459a9b7ea72c6f1e95dcf6c4
Ahmad Wijaya <cibofdev@gmail.com>
2024-04-03 11:45:10 +0700

    creating a ContentChildren decorator

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentInit, ContentChildren } from '@angular/core';
+import { TabComponent } from "../tab/tab.component";
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.css']
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs = {}
 
+  ngAfterContentInit() {
+    console.log(this.tabs)
+  }
 }
